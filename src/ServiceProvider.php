8ece59d476226e830780cd9e35fb413b1d775622
Jack Sleight <jacksleight@gmail.com>
2023-03-17 10:29:41 +0000

    WIP

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,11 +7,15 @@ use Statamic\Providers\AddonServiceProvider;
 class ServiceProvider extends AddonServiceProvider
 {
     protected $routes = [
-        'cp'  => __DIR__.'/../routes/cp.php',
+        'cp' => __DIR__.'/../routes/cp.php',
     ];
 
-    protected $scripts = [
-        __DIR__.'/../dist/js/addon.js',
+    protected $vite = [
+        'hotFile' => __DIR__.'/../vite.hot',
+        'publicDirectory' => 'dist',
+        'input' => [
+            'resources/js/addon.js',
+        ],
     ];
 
     protected $fieldtypes = [
