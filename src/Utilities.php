327412e09fe16c9395c384c3a784b2bf236b6eb1
Jack Sleight <jacksleight@gmail.com>
2023-05-04 10:55:29 +0100

    WIP

--- src/Utilities.php
+++ src/Utilities.php
@@ -2,9 +2,9 @@
 
 namespace JackSleight\StatamicFocalLink;
 
-use Statamic\Support\Str;
 use Statamic\Facades\Asset;
 use Statamic\Facades\Entry;
+use Statamic\Support\Str;
 
 class Utilities
 {
@@ -22,9 +22,9 @@ class Utilities
         }
 
         $spec = [
-            'queries'    => null,
-            'fragments'  => null,
-            'discovery'  => null,
+            'queries' => null,
+            'fragments' => null,
+            'discovery' => null,
             'discovered' => false,
         ];
 
@@ -95,13 +95,13 @@ class Utilities
         }
 
         return [
-            'value'    => $value,
-            'option'   => $option,
-            'type'     => $type,
-            'link'     => $link,
-            'query'    => $query,
+            'value' => $value,
+            'option' => $option,
+            'type' => $type,
+            'link' => $link,
+            'query' => $query,
             'fragment' => $fragment,
-            'id'       => $id,
+            'id' => $id,
         ];
     }
 
